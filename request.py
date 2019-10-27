6eecf99bd9c9c1ca71e3be4f565a61c5ae590e3c
Tony <pysolver33@gmail.com>
2019-10-27 16:59:01 +0800

    messing about

--- request.py
+++ request.py
@@ -8,13 +8,22 @@ from requests.exceptions import HTTPError
 # elif response.status_code == 404:
 #     print(str(response )+ 'Not Found.')
 
-for url in ['https://api.github.com', 'https://api.github.com/invalid']:
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except  HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-    except Exception as err:
-        print(f'Other error occurred: {err}')
-    else:
-        print('Success!')
+j = 8
+l = 12
+k = 10
+
+for i in (j,  k, l):
+    print(i)
+
+# for url in ['https://api.github.com', 'https://api.github.com/invalid']:
+#     try:
+#         response = requests.get(url)
+#         response.raise_for_status()
+#         print(response.content)
+#         print(response.json())
+#     except  HTTPError as http_err:
+#         print(f'HTTP error occurred: {http_err}')
+#     except Exception as err:
+#         print(f'Other error occurred: {err}')
+#     else:
+#         print('Success!')
