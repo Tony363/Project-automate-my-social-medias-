1a4d526a72bd70a51dc1235299db110d9a2647c3
Tony <pysolver33@gmail.com>
2019-10-27 17:16:50 +0800

    testing

--- request.py
+++ request.py
@@ -8,22 +8,29 @@ from requests.exceptions import HTTPError
 # elif response.status_code == 404:
 #     print(str(response )+ 'Not Found.')
 
-j = 8
-l = 12
-k = 10
+# j = 8
+# l = 12
+# k = 10
 
-for i in (j,  k, l):
-    print(i)
+# for i in (j,  k, l):
+#     print(i)
 
-# for url in ['https://api.github.com', 'https://api.github.com/invalid']:
-#     try:
-#         response = requests.get(url)
-#         response.raise_for_status()
-#         print(response.content)
-#         print(response.json())
-#     except  HTTPError as http_err:
-#         print(f'HTTP error occurred: {http_err}')
-#     except Exception as err:
-#         print(f'Other error occurred: {err}')
-#     else:
-#         print('Success!')
+# person = {'name': 'Eric', 'age': 74, 'fuck': 'this', 'what':'is what?'}
+# print("Hello, {name}. You are {age}. fuck {fuck}. This {what}".format(**person))
+
+
+for url in ['https://api.github.com', 'https://api.github.com/invalid']:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.headers)
+        print(response.headers['Content-Type'])
+        # print(response.content)
+        # print(response.text)
+    #     print(response.json())
+    except  HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        print('Success!')
